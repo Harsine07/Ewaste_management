c38c093ccac8ee63331c2c889eeb9e481b3f3bc1
Harsine07 <harsineku.22aim@kongu.edu>
2024-11-13 10:10:48 +0530

    demo

--- Ewaste/src/components/Customer/CustomerLogin/CustomerLogin.jsx
+++ Ewaste/src/components/Customer/CustomerLogin/CustomerLogin.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Customerlogin.css';
+import './CustomerLogin.css';
 import { useNavigate } from 'react-router-dom';
 import { auth, provider } from './firebase'; 
 import { signInWithPopup } from 'firebase/auth'; 
